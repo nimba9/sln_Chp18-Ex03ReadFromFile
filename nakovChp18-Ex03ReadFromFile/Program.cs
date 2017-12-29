b53f3daede52c163fb8f0d208d76679dffc5695b
vancisin peter <vancisin.peter@gmail.com>
2017-12-29 15:25:05 +0100

    1st iteration

--- nakovChp18-Ex03ReadFromFile/Program.cs
+++ nakovChp18-Ex03ReadFromFile/Program.cs
@@ -2,14 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace nakovChp18_Ex03ReadFromFile
+namespace CountInFile
 {
-    class Program
+    class File_WordCounter
     {
         static void Main(string[] args)
         {
+            string filePath = Console.ReadLine();
+            string text = string.Empty;
+            try
+            {
+                text = File.ReadAllText(filePath, Encoding.ASCII);
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("File not found.");
+                return;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Directory not found.");
+                return;
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("No path specified.");
+                return;
+            }
+
+            Dictionary<string, int> dictio = new Dictionary<string, int>();
+            string[] wordsArr = text.Split(new char[] { ' ', ',', '!', '.', '?', '-' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string unit in wordsArr)
+            {
+                string lowerWord = unit.ToLower();
+                if (dictio.ContainsKey(lowerWord))
+                {
+                    dictio[lowerWord]++;
+                }
+                else
+                {
+                    dictio.Add(lowerWord, 1);
+                }
+            }
+
+            foreach (KeyValuePair<string, int> word in dictio.OrderBy(word => word.Value).ThenBy(word => word.Key))
+            {
+                Console.WriteLine("{0} -> {1} times", word.Key, word.Value);
+            }
         }
     }
 }
